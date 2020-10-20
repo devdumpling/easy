54e57705aafcdec4a60aa6978ff472cb5352c52a
negtrooper1 <devon.wellsa@gmail.com>
2020-10-20 13:56:20 -0400

    added search results as separate component

---
+++ components/search-results.js
@@ -0,0 +1,23 @@
+import ArchivePreview from './archive-preview'
+
+export default function SearchResults({ posts }) {  
+  return (
+    <section className="w-full">      
+      <div className="grid grid-cols-1 md:col-gap-16 lg:col-gap-32 row-gap-10 md:row-gap-4 mb-8">
+        {posts.map((post) => (
+          <ArchivePreview
+            key={post.slug}
+            title={post.title}
+            date={post.date}
+            slug={post.slug}
+            excerpt={post.excerpt}
+            subtitle={post.subtitle}
+            readTime={post.readTime}
+            draft={post.draft}
+            publication={post.publication}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
